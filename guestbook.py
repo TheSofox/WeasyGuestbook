f08e185343a14228fe0492c5f6f464514a2ab6c7
Sofox <sofox@SofoxDesktop.(none)>
2014-01-23 02:56:30 +0000

    Added function for adding and deleting users (fixing bugs). Added date formatting. Added links for easy Admin navigation. Added link for Admin logout. Made successful login automatically direct to front page. Added button to enable admins to delete posts. Fixed bugs when deleting multiple posts.

--- guestbook.py
+++ guestbook.py
@@ -8,6 +8,16 @@ entries = []
 entry_names = []
 entry_dates = []
 
+def add_guest_post(name, entry):
+    entry_names.append(name)
+    entries.append(entry or '<blank>')
+    entry_dates.append(datetime.datetime.now())
+    
+def delete_guest_post(index):
+    del entries[index]
+    del entry_names[index]
+    del entry_dates[index]
+
 
 @app.route('/')
 def index():
@@ -16,34 +26,41 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    entries.append(request.form['entry'] or '<blank>')
     if not request.form['name']:
         flash("you must provide a name!")
         return redirect('/')
-    entry_names.append(request.form['name'])
-    entry_dates.append(datetime.datetime.now())
+    add_guest_post(request.form['name'], request.form['entry']);
     return redirect('/')
 
 @app.route('/admin')
 def admin_login():
     return render_template('login.html')
 
+
+@app.route('/logout')
+def admin_logout():
+    session['logged_in'] = False
+    return redirect('/')
+
 @app.route('/admin', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password':
         session['logged_in'] = True
+        return redirect('/')
     else:
         flash('wrong password!')
-    return admin_login()
+        return admin_login()
 
 @app.route('/moderate', methods=['POST'])
 def moderate_posts():
+    marked_posts = []
     for key in request.form:
         if not key.startswith('delete_'):
             continue
-        index = int(key.partition('_')[2])
-        del entries[index]
-        del entry_names[index]
+        marked_posts.append(int(key.partition('_')[2]))
+    marked_posts.sort(reverse=True)
+    for index in marked_posts:
+        delete_guest_post(index)
     return redirect('/')
 
 
